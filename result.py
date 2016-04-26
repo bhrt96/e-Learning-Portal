bab775d0522f91284f49e789130cdeb2ae2d15ac
badasshumor <iit2014157@iiita.ac.in>
2016-04-27 00:29:20 +0530

    Added files via upload

--- result.py
+++ result.py
@@ -6,36 +6,6 @@ import MySQLdb
 print "Content-type:text/html\r\n\r\n"
 print '<html>'
 
-
-print '<head>'
-
-#script for pie chart
-
-print """<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-
-        var data = google.visualization.arrayToDataTable([
-          ['Response', 'Number'],
-          ['Correct',   %d],
-          ['Not attempted', %d],
-          ['Wrong',%d]
-        ]);
-
-        var options = {
-          title: 'Detailed Marksheet'
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-        chart.draw(data, options);
-      }
-    </script>""" % (correct,not_att,wrong)
-
-print '</head>'
-
 print '<body>'
 
 
@@ -49,9 +19,10 @@ k=int(form.getvalue('rows'))
 
 #inserting values in studentanswer table from form
 for i in range(1,k):
-	sql="""insert into studentanswer(studentId,questionId,answer) values (%d,'%d','%s')""" % (14,int(form.getvalue('qid[%d]'%i)) ,form.getvalue('q[%d]'%i))
+	sql="""insert into studentanswer(studentId,questionId,answer) values ( %d,'%d','%s')""" % (14,int(form.getvalue('qid[%d]'%i)) ,form.getvalue('q[%d]'%i))
 	cursor.execute(sql)
 	db.commit()	
+
 
 #setting points to 4 for correct answer in studentanswer table
 sql2="""update studentanswer as sa 
@@ -63,15 +34,15 @@ cursor.execute(sql2)
 sql3= "update studentanswer set points=0 where answer='none' "
 cursor.execute(sql3)
                                 
-
-sql4= """select subject from questionbank where questionId=%d"""%(qid[1])
+sql4= """select subject from questionbank where questionId=%d"""%int(form.getvalue('qid[1]'))
 cursor.execute(sql4)
-sub=cursor.fetchall()
+sub=cursor.fetchone()
+
 
 #obtaining marks scored
 sql5="""select sum(points) from studentanswer as sa join questionbank as qb on sa.questionId=qb.questionId where studentId=%d"""%(14)
 cursor.execute(sql5)
-marks=cursor.fetchall()
+marks=cursor.fetchone()
 
 #obtaining max marks
 sql6="""select * from studentanswer as sa join questionbank as qb on sa.questionId=qb.questionId where studentId=%d"""%(14)
@@ -86,8 +57,8 @@ if(perc > 33.0):
 else:
 	result="fail"
 
-#inserting values in result table
-sql8= """insert into result(StudentId,subject,marks,'max marks',percent,result) values(%d,%s,%d,%d,%f,%s)"""%(14,sub[0],marks[0],mm,perc,result)
+# #inserting values in result table
+sql8= """insert into result(StudentId,subject,marks,max_marks,percent,result,exmaId) values('%d','%s','%d','%d',%f,'%s',)"""%(14,sub[0],marks[0],mm,perc,result)
 cursor.execute(sql8)
 
 #calculating number of correct answers
@@ -103,11 +74,40 @@ not_att=cursor.rowcount
 #calculating number of wrong answers
 sql10="""select * from studentanswer as sa join questionbank as qb on sa.questionId=qb.questionId where studentId=%d and points=%d"""%(14,-1)
 cursor.execute(sql10)
-wrong=cursor.rowcount(sql10)
+wrong=cursor.rowcount
 
 db.commit()	
 
-print result
+#script for pie chart
 
+print """<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Response', 'Number'],
+          ['Correct',%d],
+          ['Not attempted',%d],
+          ['Wrong',%d]
+        ]);
+
+        var options = {
+          title: 'Detailed Marksheet'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+    </script>"""% (correct,not_att,wrong)
+
+print """<div id="piechart" style="width: 900px; height: 500px;"></div>"""
+
+
+
+
+print result
 print "</body>"
 print "</html>"
